18cb5583443207ac3c85d3f81801bf839563adf1
alexboystudent-coder <alexboystudent@gmail.com>
2026-09-02 07:14:03 +0200

    Update j.js

--- j.js
+++ j.js
@@ -1,4 +1,4 @@
-Document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     const schedule = {
         0: {
             title: "💥 HÉTFŐ: PUSH + BICEPSZ",
